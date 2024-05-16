78e9c68f4e7a7e2ee68f1436be148145e4b62bbf
christian ochenehi peter <christianochenehipeter@gmail.com>
2024-05-16 10:55:37 +0100

    all

--- backend/api/categories/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import *
-from rest_framework import serializers
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ['id', 'name', 'created_at', 'updated_at']
